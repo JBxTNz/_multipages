b3694993e8fb7e7eb3995abf8b9d1f5976bc7aaa
JBxTNz <worathep.submit_work@hotmail.com>
2024-10-23 22:19:31 +0700

    test img 8

--- src/pages/Animation/Animation.jsx
+++ src/pages/Animation/Animation.jsx
@@ -168,166 +168,133 @@
 // };
 
 // export default Ball;
+
+
 import React, { useState, useEffect } from 'react';
 import './Animation.css';
 
+import basketball from '../../../public/img/basketball.png';
+import football from '../../../public/img/football.png';
+import cartoon from '../../../public/img/amongus-fun.jpg';
+import human from '../../../public/img/WorathepTara.png';
+import volleyball from '../../../public/img/volleyball.png';
+import logo from '../../../public/img/Iboss.png';
 
-const Ball = () => {
-    const fieldWidth = 600;
-    const fieldHeight = 400;
-    const diameter = 80;
-    const maxLeft = fieldWidth - diameter - 2;
-    const maxTop = fieldHeight - diameter - 2;
-    const vx = 5;
-    const vy = 5;
-    const rotationSpeed = 5;
 
-    const [running, setRunning] = useState(false);
-    const [goRight, setGoRight] = useState(true);
-    const [goDown, setGoDown] = useState(true);
-    const [x, setX] = useState(0);
-    const [y, setY] = useState(0);
-    const [rotation, setRotation] = useState(0);
-    const [selectedButton, setSelectedButton] = useState('None');
 
-    useEffect(() => {
-        const handleKeyDown = (event) => {
-            switch (event.key) {
-                case ' ':
-                    runClick();
-                    break;
-                case '0':
-                    setNone();
-                    break;
-                case '1':
-                    setBasketball();
-                    break;
-                case '2':
-                    setFootball();
-                    break;
-                case '3':
-                    setVolleyball();
-                    break;
-                case '4':
-                    setHuman();
-                    break;
-                case '5':
-                    setCartoon();
-                    break;
-                case '6':
-                    setLogo();
-                    break;
-                default:
-                    break;
-            }
-        };
 
-        document.addEventListener('keydown', handleKeyDown);
-        return () => document.removeEventListener('keydown', handleKeyDown);
-    }, []);
 
-    useEffect(() => {
-        const interval = setInterval(() => {
-            if (running) {
-                calculate();
-            }
-        }, 25);
-        return () => clearInterval(interval);
-    }, [running, x, y, goRight, goDown]);
+const App = () => {
+  // Constants
+  const fieldWidth = 700;
+  const fieldHeight = 400;
+  const diameter = 100;
+  const maxLeft = fieldWidth - diameter;
+  const maxTop = fieldHeight - diameter;
+  const vx = 5;
+  const vy = 5;
+  const rotationSpeed = 5; // Degrees per frame
 
-    const runClick = () => setRunning(!running);
+  // State variables
+  const [running, setRunning] = useState(false);
+  const [goRight, setGoRight] = useState(true);
+  const [goDown, setGoDown] = useState(true);
+  const [x, setX] = useState(0);
+  const [y, setY] = useState(0);
+  const [rotation, setRotation] = useState(0);
+  const [backgroundImage, setBackgroundImage] = useState('none');
 
-    const calculate = () => {
-        let newX = x;
-        let newY = y;
-        let newGoRight = goRight;
-        let newGoDown = goDown;
+  const calculatePosition = () => {
+    let newX = x;
+    let newY = y;
+    let newGoRight = goRight;
+    let newGoDown = goDown;
+    let newRotation = rotation;
 
-        if (goRight) {
-            newX += vx;
-            if (newX >= maxLeft) newGoRight = false;
-        } else {
-            newX -= vx;
-            if (newX <= 0) newGoRight = true;
-        }
+    if (goRight) {
+      newX += vx;
+      if (newX >= maxLeft) {
+        newGoRight = false;
+        newRotation += 180;
+      }
+    } else {
+      newX -= vx;
+      if (newX <= 0) {
+        newGoRight = true;
+        newRotation += 180;
+      }
+    }
 
-        if (goDown) {
-            newY += vy;
-            if (newY >= maxTop) newGoDown = false;
-        } else {
-            newY -= vy;
-            if (newY <= 0) newGoDown = true;
-        }
+    if (goDown) {
+      newY += vy;
+      if (newY >= maxTop) {
+        newGoDown = false;
+        newRotation += 180;
+      }
+    } else {
+      newY -= vy;
+      if (newY <= 0) {
+        newGoDown = true;
+        newRotation += 180;
+      }
+    }
 
-        setX(newX);
-        setY(newY);
-        setGoRight(newGoRight);
-        setGoDown(newGoDown);
-        setRotation(rotation + rotationSpeed);
-    };
+    setX(newX);
+    setY(newY);
+    setGoRight(newGoRight);
+    setGoDown(newGoDown);
+    setRotation(newRotation);
+  };
 
-    const updateButtonHighlight = (buttonName) => setSelectedButton(buttonName);
+  const runClick = () => {
+    setRunning(!running);
+  };
 
-    const setNone = () => updateButtonHighlight('None');
-    const setBasketball = () => updateButtonHighlight('Basketball');
-    const setFootball = () => updateButtonHighlight('Football');
-    const setVolleyball = () => updateButtonHighlight('Volleyball');
-    const setHuman = () => updateButtonHighlight('Human');
-    const setCartoon = () => updateButtonHighlight('Cartoon');
-    const setLogo = () => updateButtonHighlight('Logo');
+  const changeBallImage = (image) => {
+    setBackgroundImage(`url(${image})`);
+  };
 
-    const getBackground = () => {
-        switch (selectedButton) {
-            case 'Basketball':
-                return basketball;
-            case 'Football':
-                return "url('../../../public/img/football.png')";
-            case 'Volleyball':
-                return "url('volleyball.png')";
-            case 'Human':
-                return "url('WorathepTara.jpg')";
-            case 'Cartoon':
-                return "url('cartoon.jpg')";
-            case 'Logo':
-                return "url('logo.png')";
-            default:
-                return "none";
-        }
-    };
-    
-    return (
-        <div id="animation-container">
-            <div id="animation-field" style={{ width: fieldWidth, height: fieldHeight }}>
-                <div
-                    id="ball"
-                    style={{
-                        width: diameter,
-                        height: diameter,
-                        backgroundImage: getBackground(),
-                        backgroundColor: 'aliceblue',
-                        backgroundSize: '100% 100%',
-                        backgroundPosition: 'center',
-                        position: 'absolute',
-                        left: x,
-                        top: y,
-                        transform: `rotate(${rotation}deg)`,
-                    }}
-                ></div>
-            </div>
-            <div id="control">
-                <button id="run" className={`btn ${running ? 'btn-warning' : 'btn-success'}`} onClick={runClick}>
-                    {running ? 'Pause' : 'Run'}
-                </button>
-                <button className={selectedButton === 'None' ? 'button-selected' : 'button-not-selected'} onClick={setNone}>None</button>
-                <button className={selectedButton === 'Basketball' ? 'button-selected' : 'button-not-selected'} onClick={setBasketball}>Basketball</button>
-                <button className={selectedButton === 'Football' ? 'button-selected' : 'button-not-selected'} onClick={setFootball}>Football</button>
-                <button className={selectedButton === 'Volleyball' ? 'button-selected' : 'button-not-selected'} onClick={setVolleyball}>Volleyball</button>
-                <button className={selectedButton === 'Human' ? 'button-selected' : 'button-not-selected'} onClick={setHuman}>Human</button>
-                <button className={selectedButton === 'Cartoon' ? 'button-selected' : 'button-not-selected'} onClick={setCartoon}>Cartoon</button>
-                <button className={selectedButton === 'Logo' ? 'button-selected' : 'button-not-selected'} onClick={setLogo}>Logo</button>
-            </div>
-        </div>
-    );
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (running) {
+        calculatePosition();
+        setRotation((prevRotation) => prevRotation + rotationSpeed);
+      }
+    }, 25);
+
+    return () => clearInterval(interval);
+  }, [running, x, y, rotation, goRight, goDown]);
+
+  return (
+    <div id="container">
+      <div id="field">
+        <div
+          id="ball"
+          style={{
+            left: `${x}px`,
+            top: `${y}px`,
+            transform: `rotate(${rotation}deg)`,
+            backgroundImage: backgroundImage,
+            backgroundSize: '100% 100%',
+            backgroundPosition: 'center',
+          }}
+        ></div>
+      </div>
+
+      <div id="control">
+        <button className={`btn ${running ? 'btn-warning' : 'btn-success'}`} onClick={runClick}>
+          <span className={`bi ${running ? 'bi-pause' : 'bi-play'}`}>&nbsp;{running ? 'PAUSE' : 'RUN'}</span>
+        </button>
+        <button className="btn btn-primary" onClick={() => changeBallImage('none')}>NONE</button>
+        <button className="btn btn-primary" onClick={() => changeBallImage(basketball)}>Basketball</button>
+        <button className="btn btn-primary" onClick={() => changeBallImage(football)}>Football</button>
+        <button className="btn btn-primary" onClick={() => changeBallImage(volleyball)}>Volleyball</button>
+        <button className="btn btn-primary" onClick={() => changeBallImage(human)}>Human</button>
+        <button className="btn btn-primary" onClick={() => changeBallImage(cartoon)}>Cartoon</button>
+        <button className="btn btn-primary" onClick={() => changeBallImage(logo)}>Logo</button>
+      </div>
+    </div>
+  );
 };
 
-export default Ball;
+export default App;
